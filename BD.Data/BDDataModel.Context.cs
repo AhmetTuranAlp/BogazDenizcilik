4ad3d11e540f7e2b984515faceda564b36956aa4
Ahmet Turan Alp <32432160+AhmetTuranAlp@users.noreply.github.com>
2020-04-23 17:54:41 +0300

    .

--- BD.Data/BDDataModel.Context.cs
+++ BD.Data/BDDataModel.Context.cs
@@ -12,8 +12,6 @@ namespace BD.Data
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-    using System.Data.Entity.Core.Objects;
-    using System.Linq;
     
     public partial class ProjeBEntities : DbContext
     {
@@ -27,7 +25,6 @@ namespace BD.Data
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<Arsiv> Arsiv { get; set; }
         public virtual DbSet<Ayarlar> Ayarlar { get; set; }
         public virtual DbSet<Cihaz> Cihaz { get; set; }
         public virtual DbSet<Ekipler> Ekipler { get; set; }
@@ -36,31 +33,5 @@ namespace BD.Data
         public virtual DbSet<Personel> Personel { get; set; }
         public virtual DbSet<SifreGirisZamanaları> SifreGirisZamanaları { get; set; }
         public virtual DbSet<Sorunlar> Sorunlar { get; set; }
-        public virtual DbSet<VeritabaniYedek> VeritabaniYedek { get; set; }
-    
-        public virtual ObjectResult<spEkipGenelPerformans_Result> spEkipGenelPerformans()
-        {
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<spEkipGenelPerformans_Result>("spEkipGenelPerformans");
-        }
-    
-        public virtual ObjectResult<Nullable<int>> spOperasyonAdet()
-        {
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<Nullable<int>>("spOperasyonAdet");
-        }
-    
-        public virtual ObjectResult<spOperasyonArsivListe_Result> spOperasyonArsivListe()
-        {
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<spOperasyonArsivListe_Result>("spOperasyonArsivListe");
-        }
-    
-        public virtual ObjectResult<spOperasyonListe_Result> spOperasyonListe()
-        {
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<spOperasyonListe_Result>("spOperasyonListe");
-        }
-    
-        public virtual ObjectResult<spPersonelGenelPerformans_Result> spPersonelGenelPerformans()
-        {
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<spPersonelGenelPerformans_Result>("spPersonelGenelPerformans");
-        }
     }
 }
